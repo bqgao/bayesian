a3b78da6a4c68648ebca8d1465f75a92210cfe33
liyushan3 <liys9378@gmail.com>
2016-12-13 23:18:20 +0100

    data analysis code

---
+++ read_index.py
@@ -0,0 +1,7 @@
+# To read CODAindex.txt, which describes the content of the CODAchain1.txt file by listing the variable names.
+
+def read_index(filename,line_num):
+	file = open(filename,'r')
+	
+
+	return
